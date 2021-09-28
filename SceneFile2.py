397788f6df0bbf2c45a2b518e3c86f542327568f
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-09-28 12:44:51 +0530

    Update SceneFile2.py

--- SceneFile2.py
+++ SceneFile2.py
@@ -1,25 +1,28 @@
 from RenderLib.RenderLib import *
 
-w,h = 1280,720
+w,h = 1600,720
 
-Obj = Mesh.LoadMesh("OBJ/sphere2.obj")
+Obj = Mesh.LoadMesh("OBJ/torus.obj")
+Obj = Mesh.LoadMesh("OBJ/sphere.obj")
 
-Obj.mat.color = Color(1,0,0)
-Obj.Rotation = [0,0,0]
+Obj.Rotation = [-10,0,0]
+Obj.Translate = [0,.3,6]
+Obj2.Translate = [0,.3,6]
 Obj.Scale = [.5]*3
-Obj.PreTranslate = [0,0,0]
-Obj.Translate = [0,0,6]
+Obj2.Scale = [.25]*3
 
-Obj.mat = Material(color=Color(1,0,.1),spec=1)
+Obj.mat = Material(color=Color(0,.2,1),spec=1)
+Obj2.mat = Material(color=Color(1,.2,0),spec=1)
 
 camr = Camera(Vec(), 50)
 
 dxlt = Light(Vec(),power=.8,dirx=Vec(0,-.51,1))
-pxlt = Light(Vec(0,2,0), 280,typ="POINT")
+pxlt = Light(Vec(0,2,0), 600,typ="POINT")
 
 
-RenderScene = Scene([Obj],[pxlt],camr,w,h)
+RenderScene = Scene([Obj,Obj2],[pxlt],camr,w,h)
 
-Anim = [0,1,0]
+Anim = [1,1,0]
+
 #RenderScene.HDRClip=True
 #RenderScene.DrwMode= "ALL"
