45450701d6d32ba69f930eb033f63499af7ed5e3
Udit Chandra <83822686+udit-uc15@users.noreply.github.com>
2021-09-30 23:27:06 +0530

    Update SceneFile.py

--- SceneFile.py
+++ SceneFile.py
@@ -1,22 +1,19 @@
-from RenderLib.RenderLib import *
+from RenderLib.RenderLibL import *
 
 w,h = 1600,720
 
 Obj = Mesh.LoadMesh("OBJ/teapot.obj")
 
 Obj.Rotation = [30,0,0]
-Obj.Scale = [1/1.3]*3
+Obj.Scale = Vec(.5, .5, .5)
 Obj.mat = Material(color=Color(0,0.2,1),spec=1)
 
-camr = Camera(Vec(), 60)
 
-
-dxlt = Light(Vec(),.8,dirx=Vec(0,-.5,1))
-pxlt = Light(Vec(0,1,0), 250,typ="POINT")
-
+camr = Camera(Vec(), 50)
+pxlt = Light(Vec(0,1,0), 550)
 
 RenderScene = Scene([Obj],[pxlt],camr,w,h)
 
 Anim = [0,-2,0]
 #RenderScene.HDRClip=True
-#RenderScene.DrwMode= "WIREVERT"
+#RenderScene.DrwMode= "WIRE"
