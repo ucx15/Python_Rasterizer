d22828aa65b7f8ad58f79f9670aeec51d80ed90e
Udit Chandra <83822686+udit-uc15@users.noreply.github.com>
2021-09-30 23:31:14 +0530

    Update SceneFile2.py

--- SceneFile2.py
+++ SceneFile2.py
@@ -6,17 +6,13 @@ Obj = Mesh.LoadMesh("OBJ/torus.obj")
 Obj2 = Mesh.LoadMesh("OBJ/sphere.obj")
 
 Obj.Rotation = [-10,0,0]
-Obj.Translate = [0,.3,6]
-Obj2.Translate = [0,.3,6]
-Obj.Scale = [.5]*3
-Obj2.Scale = [.25]*3
+Obj.Scale = Vec(.5, .5, .5)
+Obj2.Scale = Vec(.25, .25, .25)
 
 Obj.mat = Material(color=Color(0,.2,1),spec=1)
 Obj2.mat = Material(color=Color(1,.2,0),spec=1)
 
 camr = Camera(Vec(), 50)
-
-dxlt = Light(Vec(),power=.8,dirx=Vec(0,-.51,1))
 pxlt = Light(Vec(0,2,0), 600,typ="POINT")
 
 
